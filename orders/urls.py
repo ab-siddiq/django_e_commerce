b424b013749d67156665112d8da661d187cf8728
Md. Abu Bakkar Siddiq <siddiq.aiub@gmail.com>
2023-08-28 08:56:17 +0600

    set up order pages

--- orders/urls.py
+++ orders/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('order_completed/',views.order_completed),
+    path('place_order/',views.place_order),
 ]
--- orders/views.py
+++ orders/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def order_completed(request):
     return render(request,'orders/order_complete.html')
+def place_order(request):
+    return render(request,'orders/place_order.html')
