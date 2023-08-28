1ab4f2879479ff79fff35fd04cfa676d87d3d0ae
Md. Abu Bakkar Siddiq <siddiq.aiub@gmail.com>
2023-08-28 08:28:35 +0600

    setting up profile page

--- accounts/views.py
+++ accounts/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def register(request):
     return render(request,'accounts/register.html')
+def profile(request):
+    return render(request,'accounts/dashboard.html')
